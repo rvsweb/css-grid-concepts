8001e036b749eea7fadfcae4607388d3bbfe5972
RVSWeb <siguelasreglas@gmail.com>
2020-01-03 22:50:24 +0100

    he arreglado la parte del articulo

--- css-grid-quackit/src/js/reloj.js
+++ css-grid-quackit/src/js/reloj.js
@@ -1,14 +1,38 @@
 
 function reloj() {
-    document.getElementById('dia').innerHTML = new Date().getDay();
-    document.getElementById('mes').innerHTML = new Date().getMonth();
+
+
+    document.getElementById('dia').innerHTML = new Date().getDate();
+    document.getElementById('mes').innerHTML = new Date().getMonth() + 1;
     document.getElementById('anio').innerHTML = new Date().getFullYear();
 
     var hora = new Date().getHours();
     var min = new Date().getMinutes();
     var seg = new Date().getSeconds();
 
+    var sgZero;
+    var minZero;
+    
+    sgZero = setZero(seg);
+    minZero = setZero(min);
+
     document.getElementById('hora-completa').innerHTML =
-        hora + ":" + min + ":" + seg;
+        hora + ":" + minZero + ":" + sgZero;
     setTimeout(reloj);
+}
+
+
+function setZero(tiempo) {
+
+    tiempo.toString();
+
+    var tiempoString = tiempo;
+    var cadenaFinal = "";
+
+    if (tiempoString < 10) {
+        cadenaFinal = "0".concat(tiempoString);
+    } else {
+        cadenaFinal = tiempo;
+    }
+    return cadenaFinal;
 }
